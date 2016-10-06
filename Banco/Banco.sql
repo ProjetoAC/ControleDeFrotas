3134d3839de2e87bebe65ad4babf544060edc5be
VanessaGodoy <vanessarodriguesgodoy@hotmail.com>
2016-10-05 21:36:16 -0300

    Banco

--- Banco/Banco.sql
+++ Banco/Banco.sql
@@ -14,30 +14,31 @@ CONSTRAINT FK_Estados_Cidades FOREIGN KEY (EstadoId)
 REFERENCES Estados (EstadoId)
 );
 
-create table pessoas(
-PessoaId integer NOT NULL,
+create table usuario(
+IdUsuario SERIAL NOT NULL,
 CidadeId integer NOT NULL,
 EstadoId integer NOT NULL,
 Nome varchar(100) NOT NULL DEFAULT '',
 Cpf integer NOT NULL,
 Telefone integer NOT NULL,
+Estado varchar(100) NOT NULL DEFAULT '',
+Cidade varchar(100) NOT NULL DEFAULT '',
 Endereco varchar(100) NOT NULL DEFAULT '',
 Bairro varchar(100) NOT NULL DEFAULT '',
-NumeroCarteiraMotorista integer NOT NULL,
+NumeroCarteira integer NOT NULL,
 TipoCarteira varchar(100) NOT NULL DEFAULT '',
-CONSTRAINT PK_Pessoas PRIMARY KEY (PessoaId),
-CONSTRAINT FK_Estados_Pessoas FOREIGN KEY (EstadoId)
+CONSTRAINT PK_Pessoas PRIMARY KEY (IdUsuario),
+CONSTRAINT FK_Estados_Usuario FOREIGN KEY (EstadoId)
 REFERENCES Estados (EstadoId),
-CONSTRAINT FK_Cidades_Pessoas FOREIGN KEY (CidadeId)
+CONSTRAINT FK_Cidades_Usuario FOREIGN KEY (CidadeId)
 REFERENCES Cidades (CidadeId)
 );
 
 create table veiculos(
-VeiculoId integer NOT NULL,
+IdVeiculo integer NOT NULL,
 TipoVeiculo varchar(50) NOT NULL DEFAULT '',
 Placa varchar(50) NOT NULL DEFAULT '',
 TipoCombustivel varchar(50) NOT NULL DEFAULT '',
 Carga varchar(100) NOT NULL DEFAULT '',
-CONSTRAINT PK_Veiculos PRIMARY KEY (VeiculoId)
+CONSTRAINT PK_Veiculos PRIMARY KEY (IdVeiculo)
 );
-
