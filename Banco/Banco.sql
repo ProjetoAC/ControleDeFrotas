4f16ed41d61160236018913b3981e11db7c7e5dd
vanessa godoy <vanessarodriguesgodoy@hotmail.com>
2016-10-17 22:52:15 -0200

    dia17

--- Banco/Banco.sql
+++ Banco/Banco.sql
@@ -7,18 +7,27 @@ Cidade varchar(60) NOT NULL DEFAULT '',
 Estado varchar(65) NOT NULL DEFAULT '',
 Endereco varchar(100) NOT NULL DEFAULT '',
 Bairro varchar(100) NOT NULL DEFAULT '',
-NumeroCarteira integer NOT NULL,
+NumeroCarteira varchar(100) NOT NULL DEFAULT '',
 TipoCarteira varchar(100) NOT NULL DEFAULT '',
 CONSTRAINT PK_Pessoas PRIMARY KEY (IdUsuario)
 );
 
-create table veiculos(
+create table veiculo(
 IdVeiculo SERIAL NOT NULL,
 TipoVeiculo varchar(50) NOT NULL DEFAULT '',
 Placa varchar(50) NOT NULL DEFAULT '',
 TipoCombustivel varchar(50) NOT NULL DEFAULT '',
 Carga varchar(100) NOT NULL DEFAULT '',
-CONSTRAINT PK_Veiculos PRIMARY KEY (IdVeiculo)
+CONSTRAINT PK_Veiculo PRIMARY KEY (IdVeiculo)
+);
+
+create table rota (
+RotaId SERIAL NOT NULL,
+Saida varchar(70) NOT NULL DEFAULT'',
+Destino varchar(70) NOT NULL DEFAULT'',
+DistanciaPercorrida varchar(20) NOT NULL DEFAULT'',
+GastosExtras varchar(30) NOT NULL DEFAULT'',
+CONSTRAINT PK_cadastroRota PRIMARY KEY (RotaId)
 );
 
 SELECT * FROM usuario
